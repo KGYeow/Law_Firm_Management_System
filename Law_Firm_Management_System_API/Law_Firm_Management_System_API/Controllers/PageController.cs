882684445ee61869209c86df213474a59f973141
Yeow Kok Guan <kokguan0819@gmail.com>
2023-11-10 17:39:32 +0800

    Update

--- Law_Firm_Management_System_API/Law_Firm_Management_System_API/Controllers/PageController.cs
+++ Law_Firm_Management_System_API/Law_Firm_Management_System_API/Controllers/PageController.cs
@@ -1,19 +1,19 @@
-﻿using Modernize_API.Authentication;
-using Modernize_API.Dto;
-using Modernize_API.Models;
-using Modernize_API.Service;
+﻿using Law_Firm_Management_System_API.Authentication;
+using Law_Firm_Management_System_API.Dto;
+using Law_Firm_Management_System_API.Models;
+using Law_Firm_Management_System_API.Service;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace Modernize_API.Controllers
+namespace Law_Firm_Management_System_API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class PageController : BaseController
     {
-        public PageController(IConfiguration configuration, UserService userService, ModernizeDBContext context) : base(configuration, userService, context)
+        public PageController(IConfiguration configuration, UserService userService, Law_Firm_Management_System_DBContext context) : base(configuration, userService, context)
         {
         }
 
