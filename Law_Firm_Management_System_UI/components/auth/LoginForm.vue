cc690d945b02b5c1a9c634cb1a9b26bb93d1c9b5
Yeow Kok Guan <kokguan0819@gmail.com>
2023-12-26 18:41:51 +0800

    Add validation to login and register form

--- Law_Firm_Management_System_UI/components/auth/LoginForm.vue
+++ Law_Firm_Management_System_UI/components/auth/LoginForm.vue
@@ -1,61 +1,83 @@
 <template>
-  <v-row class="d-flex mb-3">
-    <v-col cols="12">
-      <v-label class="font-weight-bold mb-1">Username</v-label>
-      <v-text-field density="compact" variant="outlined" hide-details color="primary" v-model="loginDetails.username"></v-text-field>
-    </v-col>
-    <v-col cols="12">
-      <v-label class="font-weight-bold mb-1">Password</v-label>
-      <v-text-field
-        :append-inner-icon="passwordVisible ? 'mdi-eye-off' : 'mdi-eye'"
-        :type="passwordVisible ? 'text' : 'password'"
-        density="compact"
-        variant="outlined"
-        hide-details
-        color="primary"
-        v-model="loginDetails.password"
-        @click:append-inner="passwordVisible = !passwordVisible"
-      ></v-text-field>
-    </v-col>
-    <v-col cols="12" class="pt-0">
-      <div class="d-flex flex-wrap align-center ml-n2">
-        <v-checkbox v-model="checkbox"  color="primary" hide-details>
-          <template v-slot:label class="text-body-1">Remeber this Device</template>
-        </v-checkbox>
-        <div class="ml-sm-auto">
-          <NuxtLink to="/"
-            class="text-primary text-decoration-none text-body-1 opacity-1 font-weight-medium">Forgot
-            Password ?</NuxtLink>
+  <v-form @submit.prevent="login">
+    <v-row class="d-flex mb-3">
+      <v-col cols="12">
+        <v-label class="font-weight-bold mb-1">Username</v-label>
+        <v-text-field
+          density="compact"
+          variant="outlined"
+          hide-details="auto"
+          color="primary"
+          :error-messages="loginDetails.username.errorMessage"
+          v-model="loginDetails.username.value"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12">
+        <v-label class="font-weight-bold mb-1">Password</v-label>
+        <v-text-field
+          :append-inner-icon="passwordVisible ? 'mdi-eye-off' : 'mdi-eye'"
+          :type="passwordVisible ? 'text' : 'password'"
+          density="compact"
+          variant="outlined"
+          hide-details="auto"
+          color="primary"
+          :error-messages="loginDetails.password.errorMessage"
+          v-model="loginDetails.password.value"
+          @click:append-inner="passwordVisible = !passwordVisible"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12" class="pt-0">
+        <div class="d-flex flex-wrap align-center ml-n2">
+          <v-checkbox v-model="checkbox"  color="primary" hide-details>
+            <template v-slot:label class="text-body-1">Remember this Device</template>
+          </v-checkbox>
+          <div class="ml-sm-auto">
+            <NuxtLink to="/"
+              class="text-primary text-decoration-none text-body-1 opacity-1 font-weight-medium">Forgot
+              Password ?</NuxtLink>
+          </div>
         </div>
-      </div>
-    </v-col>
-    <v-col cols="12" class="pt-0">
-      <v-btn color="primary" size="large" block flat @click="login">Sign in</v-btn>
-    </v-col>
-  </v-row>
+      </v-col>
+      <v-col cols="12" class="pt-0">
+        <v-btn color="primary" size="large" type="submit" block flat>Sign in</v-btn>
+      </v-col>
+    </v-row>
+  </v-form>
 </template>
 
 <script setup>
+import { useField, useForm } from 'vee-validate'
+
 // Data
 const { signIn } = useAuth()
 const passwordVisible = ref(false)
 const checkbox = ref(true)
+const { handleSubmit } = useForm({
+  validationSchema: {
+    username(value) {
+      return value ? true : 'Username is required'
+    },
+    password(value){
+      return value ? true : 'Password is required'
+    }
+  }
+})
 const loginDetails = ref({
-  username: null,
-  password: null,
+  username: useField('username'),
+  password: useField('password'),
 })
 
 // Methods
-const login = async() => {
+const login = handleSubmit(async(values) => {
   try {
-    const result = await fetchData.$post("/Authenticate/LoginInfo", loginDetails.value)
+    const result = await fetchData.$post("/Authenticate/LoginInfo", values)
     if (!result.error) {
-      await signIn(loginDetails.value, { callbackUrl: "/" })
+      await signIn(values, { callbackUrl: "/" })
       ElNotification.success({ message: result.message })
     }
     else {
       ElNotification.error({ message: result.message })
     }
   } catch { ElNotification.error({ message: "There is a problem with the server. Please try again later." }) }
-}
+})
 </script>
--- Law_Firm_Management_System_UI/components/auth/RegisterForm.vue
+++ Law_Firm_Management_System_UI/components/auth/RegisterForm.vue
@@ -1,51 +1,83 @@
 <template>
-  <v-row class="d-flex mb-3">
-    <v-col cols="12">
-      <v-label class="font-weight-bold mb-1">Username</v-label>
-      <v-text-field density="compact" variant="outlined" hide-details color="primary" v-model="registrationDetails.username"></v-text-field>
-    </v-col>
-    <v-col cols="12">
-      <v-label class="font-weight-bold mb-1">Email Address</v-label>
-      <v-text-field density="compact" variant="outlined" type="email" hide-details color="primary" v-model="registrationDetails.email"></v-text-field>
-    </v-col>
-    <v-col cols="12">
-      <v-label class="font-weight-bold mb-1">Password</v-label>
-      <v-text-field
-        :append-inner-icon="passwordVisible ? 'mdi-eye-off' : 'mdi-eye'"
-        :type="passwordVisible ? 'text' : 'password'"
-        density="compact"
-        variant="outlined"
-        hide-details
-        color="primary"
-        v-model="registrationDetails.password"
-        @click:append-inner="passwordVisible = !passwordVisible"
-      ></v-text-field>
-    </v-col>
-    <v-col cols="12" >
-      <v-btn color="primary" size="large" block flat @click="register">Sign up</v-btn>
-    </v-col>
-  </v-row>
+  <v-form @submit.prevent="register">
+    <v-row class="d-flex mb-3">
+      <v-col cols="12">
+        <v-label class="font-weight-bold mb-1">Username</v-label>
+        <v-text-field
+          density="compact"
+          variant="outlined"
+          hide-details="auto"
+          color="primary"
+          :error-messages="registrationDetails.username.errorMessage"
+          v-model="registrationDetails.username.value"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12">
+        <v-label class="font-weight-bold mb-1">Email Address</v-label>
+        <v-text-field
+          density="compact"
+          variant="outlined"
+          type="email"
+          hide-details="auto"
+          color="primary"
+          :error-messages="registrationDetails.email.errorMessage"
+          v-model="registrationDetails.email.value"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12">
+        <v-label class="font-weight-bold mb-1">Password</v-label>
+        <v-text-field
+          :append-inner-icon="passwordVisible ? 'mdi-eye-off' : 'mdi-eye'"
+          :type="passwordVisible ? 'text' : 'password'"
+          density="compact"
+          variant="outlined"
+          hide-details="auto"
+          color="primary"
+          :error-messages="registrationDetails.password.errorMessage"
+          v-model="registrationDetails.password.value"
+          @click:append-inner="passwordVisible = !passwordVisible"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12" >
+        <v-btn color="primary" size="large" type="submit" block flat>Sign up</v-btn>
+      </v-col>
+    </v-row>
+  </v-form>
 </template>
 
 <script setup>
+import { useField, useForm } from 'vee-validate'
+
 // Data
 const { signIn } = useAuth()
 const passwordVisible = ref(false)
+const { handleSubmit } = useForm({
+  validationSchema: {
+    username(value) {
+      return value ? true : 'Username is required'
+    },
+    email(value) {
+      return value ? true : 'Email address is required'
+    },
+    password(value){
+      return value ? true : 'Password is required'
+    }
+  }
+})
 const registrationDetails = ref({
-  username: null,
-  email: null,
-  password: null,
+  username: useField('username'),
+  email: useField('email'),
+  password: useField('password'),
 })
 
 // Methods
-const register = async() => {
+const register = handleSubmit(async(values) => {
   try {
-    const result = await fetchData.$post("/Authenticate/Register", registrationDetails.value)
-
+    const result = await fetchData.$post("/Authenticate/Register", values)
     if (!result.error) {
       await signIn({
-        username: registrationDetails.value.username,
-        password: registrationDetails.value.password
+        username: values.username,
+        password: values.password
       }, { callbackUrl: "/" })
       ElNotification.success({ message: result.message })
     }
@@ -53,5 +85,5 @@ const register = async() => {
       ElNotification.error({ message: result.message })
     }
   } catch { ElNotification.error({ message: "There is a problem with the server. Please try again later." }) }
-}
+})
 </script>
